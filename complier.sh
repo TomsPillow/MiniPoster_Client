ec83cdec9d3abf596cfdaaa277f60c29122f1ba3
TomsPillow <57435613+samona-omo@users.noreply.github.com>
2021-08-25 01:33:29 +0800

    Add files via upload

---
+++ complier.sh
@@ -0,0 +1,2 @@
+cd src/
+gcc main.c util.c fileopts.c -o ../bin/client
---
+++ run.sh
@@ -0,0 +1,6 @@
+# ./bin/client -f $file_name -a $server_ip
+# e.g. (note: 123.56.23.24 is an invaild server ip address)
+# ./bin/client -f Linux_System_Programing_By_Hit.pdf -a 123.56.23.24
+
+# local_test:
+./bin/client -f Linux_System_Programing_By_Hit.pdf -a 127.0.0.1
